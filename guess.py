cf9564817b39464b84259d8e809089618c559193
Hashimrhmnp <hashimrahmanrh@gmail.com>
2023-06-05 22:16:38 +0530

    Edit

--- guess.py
+++ guess.py
@@ -1,32 +1,22 @@
 import random
 import math
-# Taking Inputs
 lower = int(input("Enter Lower bound:- "))
 
-# Taking Inputs
 upper = int(input("Enter Upper bound:- "))
 
-# generating random number between
-# the lower and upper
 x = random.randint(lower, upper)
 print("\n\tYou've only 3 chances to guess the integer!\n")
 
-# Initializing the number of guesses.
 count = 0
 
-# for calculation of minimum number of
-# guesses depends upon range
 while count < 3:
 	count += 1
 
-	# taking guessing number as input
 	guess = int(input("Guess a number:- "))
 
-	# Condition testing
 	if x == guess:
 		print("Congratulations you did it in ",
 			count, " try\n")
-		# Once guessed, loop will break
 		break
 	elif x > guess:
 		print("You guessed too small!")
@@ -37,5 +27,3 @@ while count < 3:
 if count >= 3 and x != guess:
 	print("\nThe number is %d" % x)
 	print("\tBetter Luck Next time!")
-
-# Better to use This source Code on pycharm!
